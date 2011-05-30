2e62f7c5ff06a6426f4b6c81da5a0cadb9130f58
Scott Chacon <schacon@gmail.com>
2011-05-29 20:27:53 -0700

    skeleton all set up

--- app.rb
+++ app.rb
@@ -1,27 +1,37 @@
 require 'sinatra'
 require 'sinatra_auth_github'
-require 'pp'
 
 class TerminalApp < Sinatra::Base
   enable :sessions
 
   set :github_options, {
-                          :secret    => ENV['GITHUB_CLIENT_SECRET'],
-                          :client_id => ENV['GITHUB_CLIENT_ID'],
-                          :scopes    => 'user,repo' # repo is need for org auth :\
-                        }
+    :secret    => ENV['GITHUB_CLIENT_SECRET'],
+    :client_id => ENV['GITHUB_CLIENT_ID'],
+    :scopes    => 'user,repo'
+  }
 
   register Sinatra::Auth::Github
 
-  helpers do
-    def repos
-      github_request("repos/show/#{github_user.login}")
+  get '/' do
+    if github_user
+      redirect '/term'
+    else
+      erb :index
     end
   end
 
-  get '/' do
+  get '/term' do
+    if github_user
+      @user = github_user
+      erb :term
+    else
+      redirect '/'
+    end
+  end
+
+  get '/auth' do
     authenticate!
-    "Hello There, #{github_user.name}!#{github_user.token}\n#{repos.inspect}"
+    redirect '/term'
   end
 
   get '/logout' do
